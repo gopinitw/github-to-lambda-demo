213b693d7bab01a08ba507d3df235d3448332e05
gopinitw <44080162+gopinitw@users.noreply.github.com>
2022-09-13 19:41:13 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -4,6 +4,8 @@ def lambda_handler(event, context):
     print("Hello from Lambda gopi!")
     print("Hello from gopi!")
     print("Hello from gv!")
+    print("Hello from ab!")
+    
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
