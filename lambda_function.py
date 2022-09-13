9d1871406b92e3747eb571ab64911cf80fbccc6e
gopinitw <44080162+gopinitw@users.noreply.github.com>
2022-09-13 18:09:48 +0530

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -1,2 +1,8 @@
-# This program prints Hello, world!
-print('Hello, world!')
+import json
+
+def lambda_handler(event, context):
+    print("Hello from Lambda!")
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
